48f080bfe8f3fc25628401759fec14ca62b2b0e0
Javier Rodriguez <javierorodriguez@gmail.com>
2015-11-30 16:00:47 +0300

    Add Handlebars

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 
 // Namespacing our application. We access all the methods
 // that express gives us through this `app` variable
 var app = express();
 
+// Handlebars templates
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 // Routing
 app.get('/', function (req, res) {
-  res.send('<h1>Hello Express</h1>')
+  // res.send('<h1>Hello Express</h1>')
+  res.render('home', {
+    title: 'Home',
+    users: ['Snoop Lion', '2Pac', 'The Yogi bear']
+  });
 });
 
 app.get('/me', function (req, res) {
